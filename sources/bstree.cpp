9f029e3b190b5dbd2b267327411aa367cf6dcf06
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2018-03-15 20:05:28 +0300

    Update bstree.cpp

--- sources/bstree.cpp
+++ sources/bstree.cpp
@@ -4,12 +4,47 @@
 using namespace std;
 using namespace BSTree;
 
-int main(int argc, char *argv[]) {
-  int *array = new int[argc-1];
-  for (int i = 1; i < argc; i++) array[i] = atoi(argv[i]);
-  Tree *tr = new Tree;
-  for (int i = 1; i < argc; i++) tr->Func(array[i]);
-  delete[] array;
-  delete tr;
-  return 0;
+Tree::Tree() { root = nullptr; }
+
+void Tree::Add(int m_data, Node *&m_root) {
+        if (nullptr == m_root) {
+                m_root = new Node;
+                m_root->data = m_data;
+                m_root->left = m_root->right = nullptr;
+        }
+        if (m_data < m_root->data) {
+                if (m_root->left != nullptr)
+                        Add(m_data, m_root->left);
+                else {
+                        m_root->left = new Node;
+                        m_root->left->left = m_root->left->right = nullptr;
+                        m_root->left->data = m_data;
+                }
+        }
+        if (m_data > m_root->data) {
+                if (m_root->right != nullptr)
+                        Add(m_data, m_root->right);
+                else {
+                        m_root->right = new Node;
+                        m_root->right->left = m_root->right->right = nullptr;
+                        m_root->right->data = m_data;
+                }
+        }
+}
+
+void Tree::Clear(Node *&m_root) {
+        if (m_root != nullptr) {
+                Clear(m_root->left);
+                Clear(m_root->right);
+                delete m_root;
+                m_root = nullptr;
+        }
+}
+
+void Tree::Insert(int m_data) {
+        Add(m_data, root);
+}
+
+Tree::~Tree() {
+        Clear(root);
 }
