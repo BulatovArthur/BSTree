7e9ef79b102cfdca270b19ee4139e0cfca10240c
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2018-03-15 20:04:27 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -3,43 +3,19 @@
 using namespace std;
 
 namespace BSTree {
-struct Node {
-  int data;
-  Node* left;
-  Node* right;
-};
-
-class Tree {
-  Node* root;
-
- public:
-  Tree() { root = nullptr; }
-  void Add(int data, Node*& SoTree) {
-    if (nullptr == SoTree) {
-      SoTree = new Node;
-      SoTree->data = data;
-      SoTree->left = SoTree->right = nullptr;
-    }
-    if (data < SoTree->data) {
-      if (SoTree->left != nullptr)
-        Add(data, SoTree->left);
-      else {
-        SoTree->left = new Node;
-        SoTree->left->left = SoTree->left->right = nullptr;
-        SoTree->left->data = data;
-      }
-    }
-    if (data > SoTree->data) {
-      if (SoTree->right != nullptr)
-        Add(data, SoTree->right);
-      else {
-        SoTree->right = new Node;
-        SoTree->right->left = SoTree->right->right = nullptr;
-        SoTree->right->data = data;
-      }
-    }
-  }
-  void Func(int data) { Add(data, root); }
-  ~Tree() { cout << "bye"; }
-};
+        struct Node {
+                int   data;
+                Node *left;
+                Node *right;
+        };
+        class Tree {
+                Node *root;
+        public:
+                Tree();
+                void Add(int m_data, Node *&m_root);
+                void Clear(Node *&m_root);
+                void Insert(int m_data);
+                ~Tree();
+        };
 }
+
