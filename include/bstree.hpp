964f0f8371c55e2132de97b0ab02554a25c289e7
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2018-03-17 17:51:13 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -14,6 +14,8 @@ namespace BSTree {
                 Tree();
                 void Add(int m_data, Node *&m_root);
                 void Clear(Node *&m_root);
+                void Print_Tree(Node *m_root, int space);
+                void Print();             
                 void Insert(int m_data);
                 ~Tree();
         };
