e5a561d56722d7c4ebfb1ac2ad8a944ccc74aff5
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2018-03-23 23:52:35 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -10,15 +10,25 @@ namespace BSTree {
         };
         class Tree {
                 Node *root;
+                void Print(Node *m_root, int space);
+                void Add(int m_data, Node *&m_root);
+                void Clear(Node *&m_root);
+                void Transversal(Node *m_root);
+                void Straight(Node *m_root);
+                void Reverse(Node *m_root);
         public:
                 Tree();
                 bool Check();
-                void Add(int m_data, Node *&m_root);
-                void Clear(Node *&m_root);
-                void Print(Node *m_root, int space);
-                void Print();             
+                void Print();
                 void Insert(int m_data);
+                void Transversal();
+                void Straight();
+                void Reverse();
                 ~Tree();
         };
 }
+
+
+
+
 
